1624f03b016a3c5c702acb2e467b3f30e8d6f354
Nova Vortex <nova@nova.nova>
2025-03-02 02:51:59 +0000

    readmap norm

--- read_map.c
+++ read_map.c
@@ -6,26 +6,23 @@
 /*   By: szaoual <szaoual@1337.ma>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 16:09:03 by szaoual           #+#    #+#             */
-/*   Updated: 2025/03/01 23:20:47 by szaoual          ###   ########.fr       */
+/*   Updated: 2025/03/02 02:51:26 by szaoual          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
 
-char **ft_realloc(char **map, int *capacity)
+char	**ft_realloc(char **map, int *capacity)
 {
-  int old_capacity;
-  char **new_map;
+	int		old_capacity;
+	char	**new_map;
 
-  old_capacity = *capacity;
-  *capacity *= 2;
-  new_map = malloc(sizeof(char *) * *capacity + 1);
-  ft_memmove(new_map, map, sizeof(char *) * old_capacity);
-  free(map);
-  return (new_map);
+	old_capacity = *capacity;
+	*capacity *= 2;
+	new_map = malloc(sizeof(char *) * (*capacity + 1));
+	ft_memmove(new_map, map, sizeof(char *) * old_capacity);
+	free(map);
+	return (new_map);
 }
 
 char	**read_map(const char *filename)
@@ -34,34 +31,38 @@ char	**read_map(const char *filename)
 	char	*line;
 	char	**map;
 	int		i;
-  int   capacity;
+	int		capacity;
 
-  capacity = 16;
+	capacity = 16;
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (perror("Error opening file"), NULL);
-	map = malloc(sizeof(char *) * capacity + 1);
+	map = malloc(sizeof(char *) * (capacity + 1));
 	if (!map)
 		return (close(fd), NULL);
 	i = 0;
-	while ((line = get_next_line(fd)))
-  {
+	line = get_next_line(fd);
+	while (line)
+	{
 		map[i++] = line;
-    if (i == capacity)
-      map = ft_realloc(map, &capacity);
-  }
+		if (i == capacity)
+			map = ft_realloc(map, &capacity);
+		line = get_next_line(fd);
+	}
 	map[i] = NULL;
 	close(fd);
 	return (map);
 }
 
-void free_map(char **map)
+void	free_map(char **map)
 {
-    int i = 0;
-    while (map[i])
-    {
-        free(map[i]);
-        i++;
-    }
-    free(map);
+	int	i;
+
+	i = 0;
+	while (map[i])
+	{
+		free(map[i]);
+		i++;
+	}
+	free(map);
 }
